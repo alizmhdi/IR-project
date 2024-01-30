2e1c467d70e76871efa728c4084c2d74734d8ea6
mehdi.alizade <mehdi.alizade@cafebazaar.ir>
2024-01-30 19:00:51 +0330

    Fix crawler

--- utils/crawler.py
+++ utils/crawler.py
@@ -7,15 +7,16 @@ import json
 API_KEY = os.environ.get('API_KEY', 'a0ce25ec0fcff1f0a4e4af53e16162cc')
 
 class Crawler():
-    def __init__(self, prof, urls, limit=1000):
+    def __init__(self, prof, urls, limit=200):
         self.prof_name = prof
         self.start_queue = urls
         self.visited = set()
         self.limit = limit
 
-    def get_page(URL):
-        payload = {'api_key': API_KEY, 'url': URL}
-        return requests.get('http://api.scraperapi.com', params=payload)
+    def get_page(self, URL):
+        payload = {'api_key': 'a0ce25ec0fcff1f0a4e4af53e16162cc', 'url': URL.strip()}
+        r = requests.get('http://api.scraperapi.com/', params=payload)
+        return r.text
 
     def get_next_url(self):
         next_url = self.start_queue.pop(0)
@@ -24,26 +25,26 @@ class Crawler():
         
     def get_page_urls(self, page):
         new_urls = []
-        soup = BeautifulSoup(page.text, "html.parser")
-        for reference in soup.find_all('div', {'class':'cl-paper-row citation-list__paper-row paper-v2-font-only'}):
+        soup = BeautifulSoup(page, "html.parser")
+        for reference in soup.find_all('div', {'class':'cl-paper-row citation-list__paper-row'}):
             try:
               url = "https://www.semanticscholar.org" + reference.find('a', {'class':'link-button--show-visited'}).get('href')
               if url not in self.visited:
                 new_urls.append(url)
             except Exception as e:
-                print(e)
+                pass
         return new_urls
     
     def get_page_content(self, page):
-        soup = BeautifulSoup(page.text, "html.parser")
+        soup = BeautifulSoup(page, "html.parser")
 
         paper_id = self.page_url.split('/')[-1]
         title = soup.find('h1', {'data-test-id': 'paper-detail-title'}).text
-        abstract = soup.find('span', {'data-test-id':'text-truncator-text'}).text
+        abstract = soup.find('meta', {'name':'description'}).get('content')
         year = soup.find('span', {'data-test-id':'paper-year'}).text
         citation_count = soup.find_all('h2', {'class':'dropdown-filters__result-count__header dropdown-filters__result-count__citations'})[0].text.split(' ')[0]
         reference_count = soup.find_all('h2', {'class':'dropdown-filters__result-count__header dropdown-filters__result-count__citations'})[1].text.split(' ')[0]
-        authors = re.findall('author={(.*)}', page.text).pop().split(' and ')
+        authors = re.findall('author={(.*)}', page).pop().split(' and ')
         card2 = soup.find('div', {'data-test-id':'reference'})
         refs = []
         for reference in card2.find_all('div', {'class':'cl-paper-row citation-list__paper-row paper-v2-font-only'}):
@@ -81,8 +82,9 @@ class Crawler():
             try:
                 crawled_content = self.get_page_content(page)
                 crawled_pages.append(crawled_content)
+                print(f'crawled {current_url}')
             except Exception as e:
-                print(e)
+                pass
             new_urls = self.get_page_urls(page)
 
             self.start_queue.extend(new_urls)
